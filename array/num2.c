9306eb22f048ad3c84fe1ff7287b87bf15d60348
Жданова Виктория <vikajdanova@gmail.com>
2024-11-26 14:49:10 +0700

    num2 ver2

--- array/num2.c
+++ array/num2.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-    int N;
-    char c;
-    
-    if (scanf("%d%c", &N, &c) != 2 || c != '\n' || N <= 0){
-        printf("N/A");
-    } else {
+	int N;
+	char c;
+		
+	if (scanf("%d%c", &N, &c) != 2 || c != '\n' || N <= 0){
+		printf("N/A");
+	} else {
 		int* arr = malloc(N * sizeof(int));
 		for(int i = 0; i < N; i++){
 			int a;
@@ -28,7 +28,7 @@ int main()
 			}
 		}
 		free(arr);
-    }
-    
-    return 0;
+	}
+		
+	return 0;
 }
